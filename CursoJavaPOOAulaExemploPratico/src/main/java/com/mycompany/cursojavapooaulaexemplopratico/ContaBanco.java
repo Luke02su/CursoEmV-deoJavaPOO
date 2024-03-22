0c1a97d88f12343dce45c290c9a9294059a3ffe9
Lucas <68867449+Luke02su@users.noreply.github.com>
2024-03-22 17:36:18 -0300

    Add files via upload

--- CursoJavaPOOAulaExemploPratico/src/main/java/com/mycompany/cursojavapooaulaexemplopratico/ContaBanco.java
+++ CursoJavaPOOAulaExemploPratico/src/main/java/com/mycompany/cursojavapooaulaexemplopratico/ContaBanco.java
@@ -56,17 +56,22 @@ public class ContaBanco {
     }
     
     public void abrirConta() {
-        this.status = true;
-        
-        if(this.tipo == "cc") {
-            this.saldo += 50.0;
-        } else if (this.tipo == "cp") {
-            this.saldo += 150.0;
+        if(!this.status) {
+            System.out.println("--- CONTA ABERTA---");
+                if(this.tipo == "cc") {
+                    this.saldo += 50.0;
+                    System.out.println("Bonificação por ter criado uma conta " + this.tipo + ": R$50.00");
+                } if (this.tipo == "cp") {
+                    this.saldo += 150.0;
+                    System.out.println("Bonificação por ter criado uma conta " + this.tipo + ": R$150.00");
+                }
         }
+        this.status = true;
+        System.out.println(" -------");
     }
     
     public void fecharConta() {
-        if(this.status == true) {
+        if(this.status) {
             if(this.saldo > 0) {
                 sacar(this.saldo);
                 this.status = false;
@@ -74,37 +79,59 @@ public class ContaBanco {
                 System.out.println("Erro: Você está em débito.");
             }
         }
+        System.out.println("--- CONTA FECHADA ---");
+        this.status = false;
     }
     
     public void depositar(double deposito) {
         if(this.status == true) {
+            System.out.println("--- DEPOSITADO ---");
             this.saldo += deposito;
+            System.out.println("Valor depositado: " + deposito);
+            System.out.println("Valor atual do saldo: " + this.saldo);
+        } else {
+            System.out.println("Crie uma conta primeiro para depositar.");
         }
+        System.out.println(" -------\n");
     }
     
     public void sacar(double saque) {
         if(this.status == true) {
             if(this.saldo > 0 && this.saldo >= saque) {
+                System.out.println("--- SACADO ---");
                 this.saldo -= saque;
-            } 
+                System.out.println("Valor sacado: " + saque);
+                System.out.println("Valor atual do saldo: " + this.saldo);
+            } else {
+                System.out.println("ERRO: Saque acima do saldo.");
+            }
+        } else {
+            System.out.println("Crie uma conta primeiro para sacar.");
         }
+        System.out.println(" -------\n");
     }
     
     public void pagarMesal() {
-        if(this.status = true) {
+        if(this.status) {
+            System.out.println("--- PAGADO MENSALIDADE ---");
             if(this.tipo == "cc") {
                 this.saldo -= 12.00; 
             } else if (this.tipo == "cp") {
                 this.saldo -= 20.00;
             }
+        } else {
+            System.out.println("Crie uma conta para pagar mensalidade.");
         }
+        System.out.println(" -------");
     }
     
     public void status() {
+        System.out.println(" --- ESTADO DA CONTA ---");
         System.out.println("Número da conta: " + this.numConta);
         System.out.println("Tipo: " + this.tipo);
         System.out.println("Dono: " + this.dono);
         System.out.println("Saldo: " + this.saldo);
         System.out.println("Aberta? " + this.status);
+        System.out.println(" -------\n");
     }
 }
--- CursoJavaPOOAulaExemploPratico/src/main/java/com/mycompany/cursojavapooaulaexemplopratico/CursoJavaPOOAulaExemploPratico.java
+++ CursoJavaPOOAulaExemploPratico/src/main/java/com/mycompany/cursojavapooaulaexemplopratico/CursoJavaPOOAulaExemploPratico.java
@@ -4,26 +4,66 @@ import java.util.Scanner;
 public class CursoJavaPOOAulaExemploPratico {
 
     public static void main(String[] args) {
+        
         Scanner scan = new Scanner(System.in);
-        ContaBanco cliente = new ContaBanco(0, "cc", "null", 0.00, false);
+        ContaBanco cliente1 = new ContaBanco(0, "null", "null", 0.00, false);
+        int opcao;
         
-        cliente.status();
-        
-        System.out.println();
-        
-        cliente.abrirConta();
-        
-        cliente.setNumConta(2); cliente.setDono("Lucas");
-        
-        System.out.println();
-
-        double saque = scan.nextDouble();
-        cliente.sacar(saque);
-        
-        cliente.status();
-  
-        
-        
-        
+        do {
+            System.out.println("1. Abrir conta\n2. Depositar\n3. Sacar\n4. Pagar mensalidade\n5. Imprimir dados\n6. Fechar conta");
+            opcao = scan.nextInt();
+            
+            switch(opcao) {
+                case 1:
+                    if(!cliente1.isStatus()) {
+                        System.out.print("Número da conta: ");
+                        int numero = scan.nextInt();
+                        cliente1.setNumConta(numero);
+                        
+                        System.out.println("Tipo cc/cp:");
+                        String tipo = scan.nextLine();
+                        scan.nextLine(); // consome a quebra de linha (evita com que pule a leitura do tipo)
+                        cliente1.setTipo(tipo);
+                        
+                        System.out.print("Dono: ");
+                        String dono = scan.nextLine();
+                        cliente1.setDono(dono);
+                        
+                        System.out.print("Saldo: ");
+                        double saldo = scan.nextDouble();
+                        cliente1.setSaldo(saldo);
+                        
+                        cliente1.abrirConta();
+                    }
+                    break;
+                case 2:
+                    if(cliente1.isStatus()) {
+                        System.out.print("Digite o valor a depositar: ");
+                        double depositar = scan.nextDouble();
+                        cliente1.depositar(depositar);
+                    } else {
+                        opcao = 0;
+                    }
+                    break;
+                case 3:
+                    if(cliente1.isStatus()) {
+                        System.out.print("Digite o valor a sacar: ");
+                        double sacar = scan.nextDouble();       
+                        cliente1.sacar(sacar);
+                    } else {
+                        opcao = 0;
+                    }
+                        break;
+                case 4:
+                    cliente1.pagarMesal();
+                    break;
+                case 5: 
+                    cliente1.status();
+                    break;
+                case 6:
+                    cliente1.fecharConta();
+                    break;
+            }
+        } while (opcao != 7);
     }
 }
