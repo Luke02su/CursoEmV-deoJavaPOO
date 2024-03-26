e4de6762d12cf966e67eca39bdfbc03d7a8b2d44
Lucas <68867449+Luke02su@users.noreply.github.com>
2024-03-26 17:45:22 -0300

    Add files via upload

---
+++ CursoPOOJavaAula07/src/main/java/com/mycompany/cursopoojavaaula07/CursoPOOJavaAula07.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cursopoojavaaula07;
+
+/**
+ *
+ * @author TI04
+ */
+public class CursoPOOJavaAula07 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
